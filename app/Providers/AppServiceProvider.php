34a1dacc14bf926837fc5716d44c7539c043ec68
grassme <lanh.ptit.tn@gmail.com>
2017-10-17 16:29:47 +0700

    commit dau tien, example2

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+
+    protected $policies = [
+        'App\Task' => 'App\Policies\TaskPolicy',
+    ];
+
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
