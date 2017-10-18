1de67d09717ce9e080b494e0972ada8e554bca30
grassme <lanh.ptit.tn@gmail.com>
2017-10-18 11:26:03 +0700

    using bower

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,9 @@
 <html lang="en">
     <head>
         <title>Laravel Quickstart - Intermediate</title>
+        <link rel="stylesheet" type="text/css" href="/assets/bootstrap/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="/assets/bootstrap/dist/css/bootstrap-theme.css">
+        <link rel="stylesheet" type="text/css" href="/assets/bootstrap/dist/css/bootstrap.min.css.map">
 
         <!-- CSS And JavaScript -->
     </head>
